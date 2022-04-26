68bc8692eea8f71a821fb4d133875356d755ba0a
Ecorralesz <estebcz@hotmail.com>
2022-04-26 08:34:18 -0500

    2nd commit

---
+++ index.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Coca cola es una bebida gaseosa increible">  
+    <meta name="keywords" content="gaseosa, bebida, cola">
+    <link rel="shotcut icon" href="img/favicon.png">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="https://kit.fontawesome.com/8a3e1a0348.js" crossorigin="anonymous"></script>
+    <title>Inicio - Coca Cola</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <a href="index.html">
+                <img src="img/logo.svg" class="logo">
+            </a>
+            <nav>
+                <a href="#inicio">Inicio</a>
+                <a href="#nosotros">Nosotros</a>
+                <a href="#servicios">Servicios</a>
+                <a href="#galeria">Galeria</a>
+                <a href="#contactenos">Contactenos</a>
+            </nav>
+            <a href="#" class="hamb"><i class="fa-solid fa-bars"></i></a>
+        </div>
+    </header>
+    <main>
+        <section id="inicio"> 
+            <img src="img/bannerprincipal.jpg">
+            <div class="bloque-inicio">
+                <h1>Bienvenidos a Coca Cola</h1>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                <a href="#nosotros" class="boton boton-rojo">Ver mas</a>
+            </div>
+        </section>
+        <section id="nosotros" class="seccion"> 
+            <div class="container">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
+                ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not 
+                only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 
+                1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus 
+                PageMaker including versions of Lorem Ipsum.</p>
+            </div>
+        </section>
+        <section id="servicios" class="seccion"> 
+            <div class="container">
+                <div class="row">
+                    <div class="columna columna-33 columna-mobile-100">
+                        <div class="bloque-servicio">
+                            <div class="bloque-img-servicio cuadrado-perfecto">
+                                <img src="img/servicio1.jpg">
+                            </div>
+                            <div class="bloque-contenido-servicio">
+                                <h3>Servicio 1</h3>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
+                                    ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a 
+                                    type specimen book. It has survived</p>
+                                <a href="#" class="boton boton-blanco">Ver mas</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="columna columna-33 columna-mobile-100">
+                        <div class="bloque-servicio">
+                            <div class="bloque-img-servicio cuadrado-perfecto">
+                                <img src="img/servicio2.jpg">
+                            </div>
+                            <div class="bloque-contenido-servicio">
+                                <h3>Servicio 2</h3>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
+                                    ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a 
+                                    type specimen book. It has survived</p>
+                                <a href="#" class="boton boton-blanco">Ver mas</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="columna columna-33 columna-mobile-100">
+                        <div class="bloque-servicio">
+                            <div class="bloque-img-servicio cuadrado-perfecto">
+                                <img src="img/servicio3.jpg">
+                            </div>
+                            <div class="bloque-contenido-servicio">
+                                <h3>Servicio 3</h3>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
+                                    ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a 
+                                    type specimen book. It has survived</p>
+                                <a href="#" class="boton boton-blanco">Ver mas</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="galeria"> 
+            <div class="container-fluid">
+                <div class="row">
+                    <div class=" columna-33 columna-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/servicio1.jpg">
+                            <h4>Imagen 1</h4>
+                        </div>
+                    </div>
+                    <div class=" columna-33 columna-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/servicio2.jpg">
+                            <h4>Imagen 2</h4>
+                        </div>
+                    </div>
+                    <div class=" columna-33 columna-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/servicio3.jpg">
+                            <h4>Imagen 3</h4>
+                        </div>
+                    </div>
+                    <div class=" columna-33 columna-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/servicio3.jpg">
+                            <h4>Imagen 4</h4>
+                        </div>
+                    </div>
+                    <div class=" columna-33 columna-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/servicio2.jpg">
+                            <h4>Imagen 5</h4>
+                        </div>
+                    </div>
+                    <div class=" columna-33 columna-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/servicio1.jpg">
+                            <h4>Imagen 6</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="contactenos" class="seccion"> 
+            <iframe width="520" height="400" frameborder="0" id="gmap_canvas" 
+            src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=%20Alajuela+()&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="columna columna-41  columna-mobile-100 empujar-58 empujar-mobile-0 sinpadding-mobile">
+                        <form action="index.php" method="post">
+                            <div class="form-block">
+                                <input type="text" name="nombre" class="form-control" placeholder="Nombre">
+                            </div>
+                            <div class="form-block">
+                                <input type="email" name="email" class="form-control" placeholder="Email">
+                            </div>
+                            <div class="form-block">
+                                <textarea name="mensaje" placeholder="Mensaje"></textarea>
+                            </div>
+                            <div class="form-block bloque-ultimo">
+                                <input type="submit" class="boton boton-negro" value="Enviar">
+                            </div>
+                            <?php
+                            header("Set-Cookie: key=value; path=.google.com/; domain=.google.com/; HttpOnly; SameSite=None");
+
+
+                                if ($_SERVER["REQUEST_METHOD"] == "POST")
+                                {
+                                    $nombre = $_POST["nombre"];
+                                    $email = $_POST["email"];
+                                    $mensaje = $_POST["mensaje"];
+
+                                    if(isset($nombre))
+                                    {
+                                        if(isset($email))
+                                        {
+                                            if(isset($mensaje))
+                                            {
+                                                $para = "carlosckareo@gmail.com";
+                                                $asunto = "Pagina Coca Cola Form";
+                                                $cuerpo = $nombre."\n".$email."\n".$mensaje;
+                                                $additional = "From: carlosckareo@gmail.com";
+
+                                                mail($para,$asunto,$cuerpo,$additional);
+
+                                            ?>
+                                                <p> Envio exitoso </p>
+                                            <?php
+                                            }
+                                        }
+                                    }
+                                }
+                            ?>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    </main>
+    <footer class="seccion">
+        <div class="container">
+            <div class="row">
+                <div class="columna columna-25 columna-mobile-100">
+                    <img src="img/logo-blanco.png" class="logo-footer">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                </div>
+                <div class="columna columna-25 columna-mobile-100">
+                    <h3>
+                        Temas relacionados
+                        <ul>
+                            <li><a href="">Tema 1</a></li>
+                            <li><a href="">Tema 2</a></li>
+                            <li><a href="">Tema 3</a></li>
+                        </ul>
+                    </h3>
+                </div>
+                <div class="columna columna-25 columna-mobile-100">
+                    <h3>
+                        Datos de contacto 
+                        <ul>
+                            <li>Cocacola@soporte.com</li>
+                            <li>Tel: 5454484364325</li>
+                            <li>Direccion: Sarchi Sarchi Sarchi <br> 20201</li>
+                        </ul>
+                    </h3>
+                </div>
+                <div class="columna columna-25 columna-mobile-100">
+                    <h3>
+                        Redes Sociales
+                        <ul class="redes">
+                            <li>
+                                <a href="">
+                                    <i class="fa-brands fa-facebook-square"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <i class="fa-brands fa-twitter-square"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <i class="fa-brands fa-instagram-square"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </h3>
+                </div>
+            </div>
+        </div>
+        <div class="barra-footer">
+            &copy; Derechos Reservados - 2020
+        </div>
+    </footer>
+    <script src="js/jquery.js"></script>
+    <script src="js/funciones.js"></script>
+</body>
+</html>
